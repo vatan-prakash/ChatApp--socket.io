af1766829e1ae3f83001d76ae7324980ce83b323
Vatan Prakash <vatanprakash867@gmail.com>
2024-07-06 16:26:30 +0530

    chatapp

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const express = require("express");
+const path=require('path');
+const app = express();
+
+const { Server } =require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection',(socket) => {
+    socket.on('user-message',(message) => {
+       io.emit("message", message);
+    });
+});
+
+
+app.use(express.static(path.resolve("./public")));
+app.get('/',(req,res)=>{
+    return res.sendFile('/public/index.html');
+});
+server.listen('9000',()=> console.log(`server stsrt at PORT:9000`));
